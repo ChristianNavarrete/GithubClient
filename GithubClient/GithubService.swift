f0274c2e40d9f90e1d7627d2d89ce5ffdc67d8dd
HoodsDream <idinkyy@gmail.com>
2015-12-17 12:22:15 -0800

    first commit

---
+++ GithubClient/GithubService.swift
@@ -0,0 +1,9 @@
+//
+//  GithubService.swift
+//  GithubClient
+//
+//  Created by HoodsDream on 11/10/15.
+//  Copyright © 2015 HoodsDream. All rights reserved.
+//
+
+import Foundation
---
+++ GithubClient/OAuthClient.swift
@@ -0,0 +1,9 @@
+//
+//  OAuthClient.swift
+//  GithubClient
+//
+//  Created by HoodsDream on 11/9/15.
+//  Copyright © 2015 HoodsDream. All rights reserved.
+//
+
+import Foundation
